33863c4335db0afe8b89e87a269d1b095e28c403
Jiahua Nian <1064890390@qq.com>
2019-05-25 16:54:25 +0800

    1.5   logout finish

--- app/src/main/java/com/dommy/tab/fragment/MeFragment.java
+++ app/src/main/java/com/dommy/tab/fragment/MeFragment.java
@@ -2,6 +2,7 @@ package com.dommy.tab.fragment;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
@@ -11,16 +12,21 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.dommy.tab.MainActivity;
 import com.dommy.tab.R;
+import com.dommy.tab.ui.LoginActivity;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * 我
  */
 public class MeFragment extends Fragment implements View.OnClickListener {
     private LinearLayout me_project, me_paper, me_patent, me_copyright;
-    private RelativeLayout me_assessment, me_setting;
+    private RelativeLayout me_assessment, me_setting,me_logout;
     private View view;
 
+    private SharedPreferences sharedPreferences;
     private CollapsingToolbarLayout collapsingToolbarLayout;
     public MeFragment() {
     }
@@ -40,6 +46,7 @@ public class MeFragment extends Fragment implements View.OnClickListener {
         me_copyright = (LinearLayout) v.findViewById(R.id.me_copyright);
         me_assessment = (RelativeLayout) v.findViewById(R.id.me_assessment);
         me_setting = (RelativeLayout) v.findViewById(R.id.me_setting);
+        me_logout=(RelativeLayout) v.findViewById(R.id.me_logout);
         setListener();
 
     }
@@ -50,15 +57,25 @@ public class MeFragment extends Fragment implements View.OnClickListener {
         me_copyright.setOnClickListener(this);
         me_assessment.setOnClickListener(this);
         me_setting.setOnClickListener(this);
+        me_logout.setOnClickListener(this);
     }
 @Override
     public void onClick(View v) {
     switch (v.getId()) {
-        case R.id.me_project:
-//            Intent calendarIntent = new Intent(getActivity(), CalendarActivity.class);
-//            startActivity(calendarIntent);
-//            break;
+        case R.id.me_logout:
+            sharedPreferences = getActivity().getSharedPreferences("loginInfo", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("haslogged", "false");
+            editor.commit();
+            startActivity(new Intent(getContext(),LoginActivity.class));
+            onDetach();
+            break;
+
         }
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
     }
 }
 
